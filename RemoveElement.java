f3c1c3890e53605a2c0ffc9b09e0aafd65001008
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-12-01 20:53:25 +0530

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -4,21 +4,21 @@
 
 
 // We cannot us another array to save the result 
-public class RemoveElement {
-    public static void main(String[] args) {
-        int val = 2;
-        int nums[]={1,2,3,2,4,3};
-        int count = 0 ;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-               nums[count]=nums[i];
-               count++;
-                System.out.println(nums.length-nums[count]);
-              }else{
-                return ;
-              }
-       
-        }
-    }
+class RemoveElement{
+  public static void main(String [] args){
+      int val=3;
+      int nums[]={2,3,3,2};
+      System.out.print(removeElement(nums,val));
+      
+  }
+  public static int removeElement(int[] nums, int val) {
+      int count=0;
+      for (int i=0;i<nums.length;i++){
+          if(nums[i]!=val){
+              nums[count]=nums[i];
+              count++;
+          }    
+      }
+      return count;
+  }
 }
-  
