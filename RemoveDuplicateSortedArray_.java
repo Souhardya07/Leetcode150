480718ac2e2e3742e2494b3a588e1fadb395fabb
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-12-01 21:37:32 +0530

    Create RemoveDuplicateSortedArray_.java

---
+++ RemoveDuplicateSortedArray_.java
@@ -0,0 +1,15 @@
+public class RemoveDuplicateSortedArray_ {
+    public static void main (String [] args){
+        int nums[]= {1,1,1,2,2,3};
+        System.out.print(removeDuplicates(nums));
+    }
+    public static int removeDuplicates(int[] nums) {
+        int i =0;
+        for (int n:nums){
+                   if(i<2 || n!=nums[i-2]){
+                     nums[i++]=n;
+                   }
+                }
+             return i;
+        }
+}
