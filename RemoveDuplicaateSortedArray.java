4fb18fc7ba36bd77c87a49ee967fb9c6438b6d37
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-12-01 20:55:19 +0530

    Update RemoveDuplicaateSortedArray.java

--- RemoveDuplicaateSortedArray.java
+++ RemoveDuplicaateSortedArray.java
@@ -3,7 +3,7 @@ public class RemoveDuplicaateSortedArray {
         int count =0;
         for (int i=0; i<nums.length;i++){
                    if(i<nums.length-1 && nums[i]==nums[i+1]){
-                    continue;
+                     continue;
                    }else{
                     nums[count]=nums[i];
                     count++;
